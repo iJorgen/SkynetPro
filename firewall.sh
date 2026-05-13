f34dac842b47edbe5c8496d7797accf034bc3859
Jörgen <52280788+iJorgen@users.noreply.github.com>
2026-05-13 08:56:15 +0200

    Update BitWire blocklist URL in firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -54,12 +54,11 @@ logmode="enabled"		# enabled | disabled
 loginvalid="disabled"	# enabled | disabled
 
 blocklist_set="     <HaGeZi>     https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/ips/tif.txt  {16}
-                    <Feodo>      https://feodotracker.abuse.ch/downloads/ipblocklist.txt  {4}
                     <CINS>       https://raw.githubusercontent.com/borestad/firehol-mirror/refs/heads/main/ciarmy.ipset  {16}
                     <IPsum>      https://raw.githubusercontent.com/stamparm/ipsum/refs/heads/master/levels/2.txt  {24}
                     <Abuse>      https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/refs/heads/main/abuseipdb-s100-1d.ipv4  {24}
                     <Tor>        https://raw.githubusercontent.com/borestad/firehol-mirror/refs/heads/main/dm_tor.ipset  {4}
-                    <BitWire>    https://raw.githubusercontent.com/bitwire-it/ipblocklist/refs/heads/main/outbound.txt  {4}"
+                    <BitWire>    https://raw.githubusercontent.com/bitwire-it/ipblocklist/refs/heads/main/out
 blocklist_ip="      45.135.180.38
                     45.135.180.177
                     212.104.141.140"
