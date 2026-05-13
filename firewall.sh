3b221d15a22da575867ffe0583916b610c98ed11
Jörgen <52280788+iJorgen@users.noreply.github.com>
2026-05-13 08:32:49 +0200

    Update blocklist_set with additional sources

--- firewall.sh
+++ firewall.sh
@@ -53,9 +53,14 @@ filtertraffic="all"		# inbound | outbound | all
 logmode="enabled"		# enabled | disabled
 loginvalid="disabled"	# enabled | disabled
 
-blocklist_set="     <HaGeZi>    https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/ips/tif.txt  {12}
-                    <IPsum>     https://raw.githubusercontent.com/stamparm/ipsum/refs/heads/master/levels/2.txt  {12}
-                    <BitWire>   https://raw.githubusercontent.com/bitwire-it/ipblocklist/refs/heads/main/outbound.txt  {4}"
+blocklist_set="     <HaGeZi>     https://raw.githubusercontent.com/hagezi/dns-blocklists/refs/heads/main/ips/tif.txt  {16}
+                    <Feodo>      https://feodotracker.abuse.ch/downloads/ipblocklist.txt  {4}
+                    <CINS>       https://raw.githubusercontent.com/borestad/firehol-mirror/refs/heads/main/ciarmy.ipset  {16}
+                    <IPsum>      https://raw.githubusercontent.com/stamparm/ipsum/refs/heads/master/levels/2.txt  {24}
+                    <Abuse>      https://raw.githubusercontent.com/borestad/blocklist-abuseipdb/refs/heads/main/abuseipdb-s100-3d.ipv4  {24}
+                    <Tor>        https://raw.githubusercontent.com/borestad/firehol-mirror/refs/heads/main/dm_tor.ipset  {4}
+                    <Proxy>      https://raw.githubusercontent.com/borestad/firehol-mirror/refs/heads/main/firehol_proxies.netset  {16}
+                    <BitWire>    https://raw.githubusercontent.com/bitwire-it/ipblocklist/refs/heads/main/outbound.txt  {4}"
 blocklist_ip="      45.135.180.38
                     45.135.180.177
                     212.104.141.140"
